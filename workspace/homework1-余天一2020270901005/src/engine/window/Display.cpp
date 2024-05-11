e6cb55a82705d525354ac34e10683981acfdb1ac
9_Extra <77313011@qq.com>
2024-05-11 18:00:05 +0800

    格式化

--- workspace/homework1-余天一2020270901005/src/engine/window/Display.cpp
+++ workspace/homework1-余天一2020270901005/src/engine/window/Display.cpp
@@ -1,21 +1,20 @@
-#include "Display.h" 
-
+#include "Display.h"
 
 Display::Display(uint32_t width, uint32_t height) {
-HINSTANCE hInstance = GetModuleHandleW(NULL);
+    HINSTANCE hInstance = GetModuleHandleW(NULL);
 
-const WNDCLASSEXW window_class = {
-    sizeof(WNDCLASSEXW),         CS_OWNDC, WindowProc, 0,           0,   hInstance, LoadIcon(NULL, IDI_WINLOGO),
-    LoadCursor(NULL, IDC_ARROW), NULL,     NULL,       L"MyWindow", NULL};
+    const WNDCLASSEXW window_class = {
+        sizeof(WNDCLASSEXW),         CS_OWNDC, WindowProc, 0,           0,   hInstance, LoadIcon(NULL, IDI_WINLOGO),
+        LoadCursor(NULL, IDC_ARROW), NULL,     NULL,       L"MyWindow", NULL};
 
-RegisterClassExW(&window_class);
-RECT rect = {100, 100, 100 + (LONG)width, 100 + (LONG)height};
-AdjustWindowRectEx(&rect, WINDOW_STYLE, false, 0);
+    RegisterClassExW(&window_class);
+    RECT rect = {100, 100, 100 + (LONG)width, 100 + (LONG)height};
+    AdjustWindowRectEx(&rect, WINDOW_STYLE, false, 0);
 
-hwnd = CreateWindowExW(0, L"MyWindow", MY_TITLE, WINDOW_STYLE, 0, 0, rect.right - rect.left, rect.bottom - rect.top,
-                       NULL, NULL, hInstance, nullptr);
+    hwnd = CreateWindowExW(0, L"MyWindow", MY_TITLE, WINDOW_STYLE, 0, 0, rect.right - rect.left, rect.bottom - rect.top,
+                           NULL, NULL, hInstance, nullptr);
 
-if (hwnd == NULL) {
-    assert(false);
-}
+    if (hwnd == NULL) {
+        assert(false);
+    }
 }
